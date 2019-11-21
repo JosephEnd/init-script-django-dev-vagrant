f8faa82a98403eb8c61e80c115de3855e9c1eac0
JosephEnd <j.endruweit@student.fontys.nl>
2019-11-22 00:56:00 +0100

    test

--- bootstrap-django.sh
+++ bootstrap-django.sh
@@ -10,8 +10,8 @@ sudo apt-get install python3-dev sqlite python-pip make build-essential libssl-d
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl gcc g++ git python3-pip -y
 # Upgrade pip naar de laatste versie
-pip3 install --upgrade pip
-pip install --upgrade pip
+python pip3 install --upgrade pip
+python3 pip3 install --upgrade pip
 # CD naar root directory and cd in vagrant directory, geef optie om directory naam op te geven
 cd / && cd vagrant && mkdir appName && cd appName
 # git init && git pull geef optie om git pull adress en token in te voeren
@@ -23,16 +23,4 @@ pyenv install
 # pip3 install pipenv
 pip3 install pipenv
 # creeer pipenv van
-pipenv install --dev --ignore-pipfile
-# curl NodeJS 12 repo
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# install NodeJS 12
-sudo apt-get install nodejs -y
-# curl Yarn repo
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# add Yarn repo
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# apt update & install yarn
-sudo apt update -y && sudo apt install yarn -y
-# yarn install
-yarn install
+pipenv install --dev
