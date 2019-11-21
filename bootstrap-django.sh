7df06c1ea43e4251977f4465d95a892de182e0ad
JosephEnd <j.endruweit@student.fontys.nl>
2019-11-22 00:12:24 +0100

    -

--- bootstrap-django.sh
+++ bootstrap-django.sh
@@ -16,7 +16,7 @@ pip install --upgrade pip
 cd / && cd vagrant && mkdir appName && cd appName
 # git init && git pull geef optie om git pull adress en token in te voeren
 git init
-git pull https://7da08f8cc09c7a2d4497f6598fa3b3863e48faff@github.com/JosephEnd/Django-Heroku-starter.git
+git pull https://b51065507e589ce2b3dea8966f33293f5737c0b2@github.com/JosephEnd/Django-Heroku-starter.git
 #install pyenv
 curl https://pyenv.run | bash
 # run pyenv
